121ed742bd7b85f872201a368296b4af850be8b6
montes8 <emontesinos812@gmail.com>
2019-01-09 08:50:28 -0500

    creabdo logica en el interactor

--- app/src/main/java/com/example/tayler_gabbi/mvp_retrofit2/intteractores/SplashInteractorImpl.kt
+++ app/src/main/java/com/example/tayler_gabbi/mvp_retrofit2/intteractores/SplashInteractorImpl.kt
@@ -5,6 +5,18 @@ import com.example.tayler_gabbi.mvp_retrofit2.interfaces.SplashInteractor
 
 class SplashInteractorImpl: SplashInteractor {
     override fun actionSplash(listener: OnSplashFinishListener) {
-        listener.splashSucces()
+
+        val background = object : Thread() {
+            override fun run() {
+                try {
+                    Thread.sleep((3*1000).toLong())
+                    listener.splashSucces()
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+
+            }
+        }
+        background.start()
     }
 }
