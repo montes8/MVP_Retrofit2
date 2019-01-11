0bbba1b3e8c15ef595e0641081e175d32d5e6a13
montes8 <emontesinos812@gmail.com>
2019-01-10 19:27:09 -0500

    obteniendo los datos recibidos

--- app/src/main/java/com/example/tayler_gabbi/mvp_retrofit2/FormularioProductoActivity.kt
+++ app/src/main/java/com/example/tayler_gabbi/mvp_retrofit2/FormularioProductoActivity.kt
@@ -2,11 +2,15 @@ package com.example.tayler_gabbi.mvp_retrofit2
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tayler_gabbi.mvp_retrofit2.api.model.Producto
 
 class FormularioProductoActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_formulario_producto)
+
+
+        val prodcutoActualisar=intent.getParcelableExtra<Producto>(DetalleProductoActivity.PRODCUTO_ACTULIZAR)
     }
 }
