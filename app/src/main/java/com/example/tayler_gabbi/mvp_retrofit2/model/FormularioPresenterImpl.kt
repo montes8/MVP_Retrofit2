2c5b4d1a6c134fedd3c47d278c4c68f41e1b974c
montes8 <emontesinos812@gmail.com>
2019-01-11 07:23:57 -0500

    creando presenter

--- app/src/main/java/com/example/tayler_gabbi/mvp_retrofit2/model/FormularioPresenterImpl.kt
+++ app/src/main/java/com/example/tayler_gabbi/mvp_retrofit2/model/FormularioPresenterImpl.kt
@@ -1,4 +1,38 @@
 package com.example.tayler_gabbi.mvp_retrofit2.model
 
-class FormularioPresenterImpl {
+import com.example.tayler_gabbi.mvp_retrofit2.interfaces.FormularioInteractor
+import com.example.tayler_gabbi.mvp_retrofit2.interfaces.OnFormularioFinishListener
+import com.example.tayler_gabbi.mvp_retrofit2.intteractores.FormularioInteractorImpl
+import com.example.tayler_gabbi.mvp_retrofit2.presenter.FormularioPresenter
+
+class FormularioPresenterImpl : FormularioPresenter,OnFormularioFinishListener {
+
+
+    var interactorformulario : FormularioInteractor = FormularioInteractorImpl()
+
+    override fun validarGuardarProducto(nombre: String, precio: Double, lote: Int, stock: Int, descripcion: String) {
+
+        interactorformulario.guardarProducto(nombre,precio,lote,stock,descripcion,this)
+    }
+
+    override fun validarActulizarproducto(id: Int, nombre: String, precio: Double, lote: Int, stock: Int, descripcion: String) {
+        interactorformulario.actualizarProducto(id,nombre,precio,lote,stock,descripcion,this)
+    }
+
+
+    override fun productoGuardado() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun productoGuardadoError() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun productoActualizado() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun productoActualizadoError() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 }
--- app/src/main/java/com/example/tayler_gabbi/mvp_retrofit2/presenter/FormularioPresenter.kt
+++ app/src/main/java/com/example/tayler_gabbi/mvp_retrofit2/presenter/FormularioPresenter.kt
@@ -1,4 +1,8 @@
 package com.example.tayler_gabbi.mvp_retrofit2.presenter
 
 interface FormularioPresenter {
+
+    fun validarGuardarProducto(nombre : String, precio : Double , lote : Int ,stock : Int , descripcion : String)
+
+    fun validarActulizarproducto(id : Int,nombre : String, precio : Double , lote : Int ,stock : Int , descripcion : String)
 }
