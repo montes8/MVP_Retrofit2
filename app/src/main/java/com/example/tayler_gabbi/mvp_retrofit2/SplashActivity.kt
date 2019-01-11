aa3d9f13a11ba42e1558a4b70e275b2af4a9c443
montes8 <emontesinos812@gmail.com>
2019-01-10 19:07:14 -0500

    agregando logica en el splash

--- app/src/main/java/com/example/tayler_gabbi/mvp_retrofit2/SplashActivity.kt
+++ app/src/main/java/com/example/tayler_gabbi/mvp_retrofit2/SplashActivity.kt
@@ -3,9 +3,12 @@ package com.example.tayler_gabbi.mvp_retrofit2
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import com.example.tayler_gabbi.mvp_retrofit2.model.SplashPresenterImpl
 import com.example.tayler_gabbi.mvp_retrofit2.presenter.SplashPresenter
 import com.example.tayler_gabbi.mvp_retrofit2.view.SplashView
+import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity(),SplashView {
 
@@ -15,6 +18,13 @@ class SplashActivity : AppCompatActivity(),SplashView {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        val anima : Animation
+        val anima2 : Animation
+
+        anima = AnimationUtils.loadAnimation(this, R.anim.ani_arriba)
+        anima2 = AnimationUtils.loadAnimation(this, R.anim.ani_abajo)
+        once.animation=anima
+        doce.animation=anima2
         splashPresenter = SplashPresenterImpl(this)
         splashPresenter!!.splashExist()
     }
