4deb0d5b142ac8a07fe1342fcf2c1e79a5269bfd
montes8 <emontesinos812@gmail.com>
2019-01-11 06:55:00 -0500

    creando metodos de la interfaces del interactor

--- app/src/main/java/com/example/tayler_gabbi/mvp_retrofit2/interfaces/FormularioInteractor.kt
+++ app/src/main/java/com/example/tayler_gabbi/mvp_retrofit2/interfaces/FormularioInteractor.kt
@@ -1,4 +1,8 @@
 package com.example.tayler_gabbi.mvp_retrofit2.interfaces
 
 interface FormularioInteractor {
+
+    fun guardarProducto(nombre : String, precio : Double , lote : Int ,stock : Int , descripcion : String , listener : OnFormularioFinishListener)
+
+    fun actualizarProducto(nombre : String, precio : Double , lote : Int ,stock : Int , descripcion : String,listener : OnFormularioFinishListener)
 }
--- app/src/main/java/com/example/tayler_gabbi/mvp_retrofit2/interfaces/OnFormularioFinishListener.kt
+++ app/src/main/java/com/example/tayler_gabbi/mvp_retrofit2/interfaces/OnFormularioFinishListener.kt
@@ -1,4 +1,8 @@
 package com.example.tayler_gabbi.mvp_retrofit2.interfaces
 
 interface OnFormularioFinishListener {
+
+    fun productoGuardado()
+
+    fun productoActualizado()
 }
